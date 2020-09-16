613455f9977a1a8b80cc836b41332c7c7ef377bc
Zahhar <Zahhar.Pertsev@ivkhk.ee>
2020-09-16 13:55:24 +0300

    Заготовка проекта

---
+++ src/sptvr19myarray2/App.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptvr19myarray2;
+
+import java.util.Random;
+
+
+/**
+ *
+ * @author Zahhar Pertsev
+ */
+class App {
+    public void run () {
+        System.out.println("--- MyArray2 ---");
+        System.out.println("Задание: ");
+        System.out.println("Создайте массив из 20 случайных четных целых\n" +
+"чисел. Вычислите среднее арифметическое\n" +
+"элементов массива без учета минимального и\n" +
+"максимального элементов массива.");
+        System.out.println("Решение: ");
+        Random random = new Random();
+        int sum = 0;
+        int rnum = 0;
+        int min = 100;
+        int max = 0;
+        
+        int[] numbers = new int[20];
+        for (int i = 0; i < numbers.length; i++) {
+            while (true);
+                rnum = random.nextInt(99-0+1)+0;
+                (if rnum % 2 == 0) {
+                    numbers[i] = rnum;
+                    break;
+            }
+        }
+        if (numbers[i] > max) max = numbers[i];
+        if (numbers[i] > max) min = numbers[i];
+        sum += numbers[i];
+        System.out.println("%4d", numbers[i]);
+    }
+        System.out.println();
+        System.out.println("Среднее арифметическое без учёта минимального(+min+) и максимального(+max+)");
+        System.out.println((double)(sum-min-max)/(numbers.length - 2));
+        System.out.println("--- end MyArray ---");
+    
+        
+  }
+    
+
